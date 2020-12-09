db2a67c3087ef28651e578ad810c3e8a52abd9f7
Straylite <49315393+Straylite@users.noreply.github.com>
2020-12-09 03:51:02 +0100

    Update credits.lua

--- extensions/credits.lua
+++ extensions/credits.lua
@@ -1,6 +1,6 @@
 function credits()
     gui.pixelText(240,0, "Made by Straylite", 0xFFFFFFFF, 0x00000000)
-    gui.pixelText(363,0, "v0.1a", 0xFFFFFFFF, 0x00000000)
+    gui.pixelText(363,0, "v0.2a", 0xFFFFFFFF, 0x00000000)
     gui.pixelText(240,7, "MinishMaker Discord:", 0xFFFFFFFF, 0x00000000)
     gui.pixelText(240,14, "https://discord.gg/yFxmScJ3zf", 0xFF6fc4f2, 0x80000000)
 end
